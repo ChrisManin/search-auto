829055556a4ea482ac37d5fa572633399ed58497
Christophe Manin <cmn@rcfsiege.local>
2019-10-09 15:00:55 +0200

    initialisation de l'application + search component

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  SPTFY_ID: '740f900d68654c9da7190a1d5abea85a',
+  SPTFY_URL: 'https://api.spotify.com/v1/search?type=artist&limit=10&client_id=' + this.clientID + '&q='
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,18 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  SPTFY_ID: '740f900d68654c9da7190a1d5abea85a',
+  SPTFY_URL: 'https://api.spotify.com/v1/search?type=artist&limit=10&client_id=' + this.clientID + '&q='
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
