5c6f6203cbcc9a72ee7ba67d368dd4f6c2353549
Christophe Manin <cmn@rcfsiege.local>
2019-10-10 10:29:35 +0200

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-  SPTFY_ID: '740f900d68654c9da7190a1d5abea85a',
-  SPTFY_URL: 'https://api.spotify.com/v1/search?type=artist&limit=10&client_id='
+  GH_URL: 'https://api.github.com/search/repositories'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  SPTFY_ID: '740f900d68654c9da7190a1d5abea85a',
-  SPTFY_URL: 'https://api.spotify.com/v1/search?type=artist&limit=10&client_id='
+  GH_URL: 'https://api.github.com/search/repositories'
 };
 
 /*
